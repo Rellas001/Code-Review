93f13b6f6cb61034af83f218ce3547dfb332d8a4
Kianc <kiancalder@gmail.com>
2020-08-14 11:13:52 -0700

    Update flag.py
    Added vars and updated if/else

--- Flags/flag.py
+++ Flags/flag.py
@@ -1,16 +1,24 @@
 # Different ways to test multiple
 # flags at once in Python
-x, y, z = 0, 1, 0
+w, x, y, z = 0, 1, 0, 3
 
-if x == 1 or y == 1 or z == 1:
+if x == 1 or y == 1 or z == 1 or w == 2:
     print('passed')
+else:
+    print("X")
 
-if 1 in (x, y, z):
+if 1 in (w, y, z):
     print('passed')
+else:
+    print("X")
 
 # These only test for truthiness:
 if x or y or z:
     print('passed')
+else:
+    print("X")
 
 if any((x, y, z)):
     print('passed')
+else:
+    print("X")
