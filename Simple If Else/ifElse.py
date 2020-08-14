b90ec154c01b45963c9ac6238c9e8a0b19d947b3
Kianc <kiancalder@gmail.com>
2020-08-14 10:59:44 -0700

    Create ifElse.py

---
+++ Simple If Else/ifElse.py
@@ -0,0 +1,14 @@
+# If and Else
+if (1 == 1):
+    print("1 = 1")
+else:
+    print ("1 ≠ 1")
+
+print("----------")
+
+if (1 != 2):
+    print("1 does not equal 2!")
+print("----------")
+
+if (1 == 1 | 1 != 2):
+    print("1 = 1 or 1 does not = 2")
