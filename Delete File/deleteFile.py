f78cf763eb56cadf81e12cd8ac36502c1493a13c
Kianc <kiancalder@gmail.com>
2020-08-14 10:53:54 -0700

    Delete deleteFile.py

--- Delete File/deleteFile.py
+++
@@ -1,6 +0,0 @@
-import os
-if os.path.exists("demofile.txt"): #Check if the file exists
-  os.remove("demofile.txt")
-  print("Removed the file!")
-else:
-  print("The file does not exist! Make sure you spelled it correctly!")
